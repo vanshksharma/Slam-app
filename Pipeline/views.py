44b4d7469753f97e8525a2ae680cf16e9c9fc245
Vansh Kumar Sharma <vns.vansh123@gmail.com>
2023-05-15 09:57:44 +0530

    Made Customer PUT API

--- Pipeline/views.py
+++ Pipeline/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from .models import Customer
 from .serializers import CustomerSerializer
 from Auth.decorators import auth_user
+from datetime import date
 
 
 class CustomerHandler(APIView):
@@ -16,7 +17,6 @@ class CustomerHandler(APIView):
     
     @auth_user
     def post(self,request,user_dict):
-        print(request.query_params)
         payload=request.data
         payload['user']=user_dict['id']
         print(payload)
@@ -29,7 +29,32 @@ class CustomerHandler(APIView):
         else:
             return Response(customer_serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
+            
+    @auth_user
+    def put(self,request,user_dict):
+        payload=request.data
+        customer_id=payload['id']
+        try:
+            customer=Customer.objects.select_related('user').get(id=customer_id)
+        except Customer.DoesNotExist:
+            return Response({'Error':"Please Enter Valid Customer ID"},
+                            status=status.HTTP_400_BAD_REQUEST)
         
+        if customer.user.id!=user_dict['id']:
+            return Response({'Error':"The Customer does not belong to the user"},
+                            status=status.HTTP_403_FORBIDDEN)
+        
+        payload['updated_at']=date.today().isoformat()
+        customer_serializer=CustomerSerializer(customer,data=payload,partial=True)
+        if customer_serializer.is_valid():
+            updated_customer=customer_serializer.save()
+            updated_customer_json=CustomerSerializer(updated_customer).data
+            return Response({'data':updated_customer_json},
+                            status=status.HTTP_200_OK)
+        else:
+            return Response(customer_serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+       
         
         
         
