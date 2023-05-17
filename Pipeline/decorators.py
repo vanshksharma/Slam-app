6e898c0573be9f63e1579e1e30e1c08c31785373
Vansh Kumar Sharma <vns.vansh123@gmail.com>
2023-05-17 18:07:19 +0530

    Completed Pipeline Module API's

--- Pipeline/decorators.py
+++ Pipeline/decorators.py
@@ -1,56 +1,79 @@
 from functools import wraps
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Customer,Address
+from .models import Customer, Address, Lead
 
 
 def auth_customer(func):
     @wraps(func)
-    def wrapper(self,request,user_dict,*args,**kwargs):
-        if request.method=='GET':
-            customer_id=request.query_params.get('id',None)
-        elif request.method=='POST':
-            customer_id=request.data.get('customer',None)
+    def wrapper(self, request, user_dict, *args, **kwargs):
+        if request.method == 'GET':
+            customer_id = request.query_params.get('customer', None)
         else:
-            customer_id=request.data.get('id',None)
-        
+            customer_id = request.data.get('customer', None)
+
         if not customer_id:
-            return Response({'Error':'No Customer ID provided'},
+            return Response({'Error': 'No Customer ID provided'},
                             status=status.HTTP_400_BAD_REQUEST)
-        
+
         try:
-            customer=Customer.objects.select_related('user').get(id=customer_id)
+            customer = Customer.objects.select_related(
+                'user').get(id=customer_id)
         except Customer.DoesNotExist:
-            return Response({'Error':"Please Enter Valid Customer ID"},
+            return Response({'Error': "Please Enter Valid Customer ID"},
                             status=status.HTTP_400_BAD_REQUEST)
-        
-        if customer.user.id!=user_dict['id']:
-            return Response({'Error':"The Customer does not belong to the user"},
+
+        if customer.user.id != user_dict['id']:
+            return Response({'Error': "The Customer does not belong to the user"},
                             status=status.HTTP_403_FORBIDDEN)
-        
-        return func(self,request,user_dict,customer,*args,**kwargs)
-    
+
+        return func(self, request, user_dict, customer, *args, **kwargs)
+
     return wrapper
 
 
 def auth_address(func):
     @wraps(func)
-    def wrapper(self,request,user_dict,*args,**kwargs):
-        address_id=request.data.get('id',None)
+    def wrapper(self, request, user_dict, *args, **kwargs):
+        address_id = request.data.get('address', None)
         if not address_id:
-            return Response({'Error':'No Address ID provided'},
+            return Response({'Error': 'No Address ID provided'},
                             status=status.HTTP_400_BAD_REQUEST)
-        
+
         try:
-            address=Address.objects.select_related('customer').get(id=address_id)
+            address = Address.objects.select_related(
+                'customer').get(id=address_id)
         except Address.DoesNotExist:
-            return Response({'Error':"Please Enter Valid Address ID"},
+            return Response({'Error': "Please Enter Valid Address ID"},
                             status=status.HTTP_400_BAD_REQUEST)
-        
-        if address.customer.user.id!=user_dict['id']:
-            return Response({'Error':"The Customer does not belong to the user"},
+
+        if address.customer.user.id != user_dict['id']:
+            return Response({'Error': "The Customer does not belong to the user"},
                             status=status.HTTP_403_FORBIDDEN)
-        
-        return func(self,request,user_dict,address,*args,**kwargs)
-    
+
+        return func(self, request, user_dict, address, *args, **kwargs)
+
+    return wrapper
+
+
+def auth_lead(func):
+    @wraps(func)
+    def wrapper(self, request, user_dict, *args, **kwargs):
+        lead_id = request.data.get('lead', None)
+        if not lead_id:
+            return Response({'Error': 'No Lead ID provided'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            lead = Lead.objects.select_related('customer').get(id=lead_id)
+        except Lead.DoesNotExist:
+            return Response({'Error': "Please Enter Valid Lead ID"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if lead.customer.user.id != user_dict['id']:
+            return Response({'Error': "The Lead does not belong to the user"},
+                            status=status.HTTP_403_FORBIDDEN)
+
+        return func(self, request, user_dict, lead, *args, **kwargs)
+
     return wrapper
--- Pipeline/views.py
+++ Pipeline/views.py
@@ -1,169 +1,274 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Customer,Address,Lead
-from .serializers import CustomerSerializer,AddressSerializer,LeadSerializer
+from .models import Customer, Address, Lead
+from .serializers import CustomerSerializer, AddressSerializer, LeadSerializer
 from Auth.decorators import auth_user
-from datetime import date
-from .decorators import auth_customer,auth_address
+from datetime import date, datetime
+from .decorators import auth_customer, auth_address, auth_lead
 from .constants import StageConstant
 
 
 class CustomerHandler(APIView):
     @auth_user
-    def get(self,request,user_dict):
-        customers=Customer.objects.select_related('user').filter(user__id=user_dict['id'])
-        customer_data=CustomerSerializer(customers,many=True).data
+    def get(self, request, user_dict):
+        customers = Customer.objects.select_related(
+            'user').filter(user__id=user_dict['id'])
+        customer_data = CustomerSerializer(customers, many=True).data
         return Response({'data': customer_data},
                         status=status.HTTP_200_OK)
-    
+
     @auth_user
-    def post(self,request,user_dict):
-        payload=request.data
-        payload['user']=user_dict['id']
-        customer_serializer=CustomerSerializer(data=payload)
+    def post(self, request, user_dict):
+        payload = request.data
+        payload['user'] = user_dict['id']
+        payload['created_at'] = date.today().isoformat()
+        payload['updated_at'] = date.today().isoformat()
+        customer_serializer = CustomerSerializer(data=payload)
         if customer_serializer.is_valid():
-            customer=customer_serializer.save()
-            customer_json=customer_serializer.data
+            customer = customer_serializer.save()
+            customer_json = customer_serializer.data
             return Response({'data': customer_json},
                             status=status.HTTP_200_OK)
         else:
             return Response(customer_serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
-            
+
     @auth_user
     @auth_customer
-    def put(self,request,user_dict,customer):
-        payload=request.data
-        payload['updated_at']=date.today().isoformat()
-        customer_serializer=CustomerSerializer(customer,data=payload,partial=True)
+    def put(self, request, user_dict, customer):
+        payload = request.data
+        if 'created_at' in payload:
+            del payload['created_at']
+        payload['updated_at'] = date.today().isoformat()
+        customer_serializer = CustomerSerializer(
+            customer, data=payload, partial=True)
         if customer_serializer.is_valid():
-            updated_customer=customer_serializer.save()
-            updated_customer_json=customer_serializer.data
-            return Response({'data':updated_customer_json},
+            updated_customer = customer_serializer.save()
+            updated_customer_json = customer_serializer.data
+            return Response({'data': updated_customer_json},
                             status=status.HTTP_200_OK)
         else:
             return Response(customer_serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
-    
+
     @auth_user
     @auth_customer
-    def delete(self,request,user_dict,customer):
+    def delete(self, request, user_dict, customer):
         customer.delete()
         return Response({'Message': 'Customer Deleted Successfully'},
-                        status=status.HTTP_200_OK)    
-        
-        
+                        status=status.HTTP_200_OK)
+
+
 class AddressHandler(APIView):
     @auth_user
     @auth_customer
-    def get(self,request,user_dict,customer):
-        address=Address.objects.select_related('customer').filter(customer__id=customer.id)
-        address_data=AddressSerializer(address,many=True).data
-        return Response({'data':address_data},
+    def get(self, request, user_dict, customer):
+        address = Address.objects.select_related(
+            'customer').filter(customer__id=customer.id)
+        address_data = AddressSerializer(address, many=True).data
+        return Response({'data': address_data},
                         status=status.HTTP_200_OK)
-    
+
     @auth_user
     @auth_customer
-    def post(self,request,user_dict,customer):
-        payload=request.data
-        adddress_serializer=AddressSerializer(data=payload)
+    def post(self, request, user_dict, customer):
+        payload = request.data
+        payload['created_at'] = date.today().isoformat()
+        payload['updated_at'] = date.today().isoformat()
+        adddress_serializer = AddressSerializer(data=payload)
         if adddress_serializer.is_valid():
-            address=adddress_serializer.save()
-            address_json=adddress_serializer.data
-            return Response({'data':address_json},
+            address = adddress_serializer.save()
+            address_json = adddress_serializer.data
+            return Response({'data': address_json},
                             status=status.HTTP_200_OK)
         else:
             return Response(adddress_serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
-    
+
     @auth_user
     @auth_address
-    def put(self,request,user_dict,address):
-        payload=request.data
-        payload['updated_at']=date.today().isoformat()
-        address_serializer=AddressSerializer(address,data=payload,partial=True)
+    def put(self, request, user_dict, address):
+        payload = request.data
+        if 'created_at' in payload:
+            del payload['created_at']
+        payload['updated_at'] = date.today().isoformat()
+        address_serializer = AddressSerializer(
+            address, data=payload, partial=True)
         if address_serializer.is_valid():
-            updated_address=address_serializer.save()
-            updated_address_json=address_serializer.data
-            return Response({'data':updated_address_json},
+            updated_address = address_serializer.save()
+            updated_address_json = address_serializer.data
+            return Response({'data': updated_address_json},
                             status=status.HTTP_200_OK)
         else:
             return Response(address_serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
-    
+
     @auth_user
     @auth_address
-    def delete(self,request,user_dict,address):
+    def delete(self, request, user_dict, address):
         address.delete()
         return Response({'Message': 'Address Deleted Successfully'},
                         status=status.HTTP_200_OK)
-            
+
 
 class LeadHandler(APIView):
     @auth_user
-    def get(self,request,user_dict):
-        leads=Lead.objects.select_related('customer').filter(customer__user__id=user_dict['id'])
-        lead_data=LeadSerializer(leads,many=True).data
-        return Response({'data':lead_data},
+    def get(self, request, user_dict):
+        leads = Lead.objects.select_related('customer').filter(
+            customer__user__id=user_dict['id'])
+        lead_data = LeadSerializer(leads, many=True).data
+        return Response({'data': lead_data},
                         status=status.HTTP_200_OK)
-    
+
     @auth_user
     @auth_customer
-    def post(self,request,user_dict,customer):
-        payload=request.data
-        stage=payload.get('stage',None)
-        closing_date=payload.get('closing_date',None)
-        confidence=payload.get('confidence',None)
-        
+    def post(self, request, user_dict, customer):
+        payload = request.data
+        stage = payload.get('stage', None)
+        closing_date = payload.get('closing_date', None)
+        confidence = payload.get('confidence', None)
+
         # Stage Check
         if not stage:
-            return Response({'Error':'Lead stage not provided'},
+            return Response({'Error': 'Lead stage not provided'},
                             status=status.HTTP_400_BAD_REQUEST)
         try:
-            stage=StageConstant[stage.upper()]
-            payload['stage']=stage.name
+            stage = StageConstant[stage.upper()]
+            payload['stage'] = stage.name
         except:
-            return Response({'Error':'Invalid Stage Provided'},
+            return Response({'Error': 'Invalid Stage Provided'},
                             status=status.HTTP_400_BAD_REQUEST)
-        
-        #Closing Date Check
-        if stage==StageConstant.OPPORTUNITY or stage==StageConstant.CONTACTED or stage==StageConstant.NEGOTIATION:
+
+        # Closing Date Check
+        if stage == StageConstant.OPPORTUNITY or stage == StageConstant.CONTACTED or stage == StageConstant.NEGOTIATION:
             if closing_date:
-                return Response({'Error':"Only leads in Closed Won or Closed Lost stage can be provided a closing date."},
+                return Response({'Error': "Only leads in Closed Won or Closed Lost stage can be provided a closing date."},
                                 status=status.HTTP_400_BAD_REQUEST)
         else:
             if not closing_date:
-                payload['closing_date']=date.today().isoformat()
-        
+                payload['closing_date'] = date.today().isoformat()
+            else:
+                try:
+                    closing_date = datetime.strptime(
+                        closing_date, "%Y-%m-%d").date()
+                    if closing_date < date.today():
+                        return Response({'Error': "Enter Valid Closing Date"},
+                                        status=status.HTTP_400_BAD_REQUEST)
+                except:
+                    return Response({'Error': "Enter Valid Closing Date"},
+                                    status=status.HTTP_400_BAD_REQUEST)
+
         # Confidence check
         if not confidence:
-            return Response({'Error':'No Confidence Level Provided'},
+            return Response({'Error': 'No Confidence Level Provided'},
                             status=status.HTTP_400_BAD_REQUEST)
         try:
-            confidence=float(confidence)
+            confidence = float(confidence)
         except:
-            return Response({'Error':'Invalid Confidence Level Provided'},
+            return Response({'Error': 'Invalid Confidence Level Provided'},
                             status=status.HTTP_400_BAD_REQUEST)
-        
-        if confidence>=0.0 and confidence<=1.0:
-            payload['confidence']=confidence
+
+        if confidence >= 0.0 and confidence <= 1.0:
+            payload['confidence'] = confidence
         else:
-            return Response({'Error':'Invalid Confidence Level Provided'},
-                        status=status.HTTP_400_BAD_REQUEST)
-        
-        lead_serializer=LeadSerializer(data=payload)
+            return Response({'Error': 'Invalid Confidence Level Provided'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        lead_serializer = LeadSerializer(data=payload)
         if lead_serializer.is_valid():
-            lead=lead_serializer.save()
-            lead_json=lead_serializer.data
-            return Response({'data':lead_json},
+            lead = lead_serializer.save()
+            lead_json = lead_serializer.data
+            return Response({'data': lead_json},
                             status=status.HTTP_200_OK)
-        
+
         else:
             return Response(lead_serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
-        
-        
-            
-        
-        
+
+    @auth_user
+    @auth_lead
+    def put(self, request, user_dict, lead):
+        payload = request.data
+        stage = payload.get('stage', None)
+        closing_date = payload.get('closing_date', None)
+        confidence = payload.get('confidence', None)
+
+        # Stage and Closing Date Check
+        if stage:
+            try:
+                stage = StageConstant[stage.upper()]
+                payload['stage'] = stage.name
+            except:
+                return Response({'Error': 'Invalid Stage Provided'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            if stage == StageConstant.OPPORTUNITY or stage == StageConstant.CONTACTED or stage == StageConstant.NEGOTIATION:
+                if closing_date:
+                    return Response({'Error': "Only leads in Closed Won or Closed Lost stage can be provided a closing date."},
+                                    status=status.HTTP_400_BAD_REQUEST)
+                if lead.closing_date:
+                    payload['closing_date'] = None
+
+            else:
+                if closing_date:
+                    try:
+                        closing_date = datetime.strptime(
+                            closing_date, "%Y-%m-%d").date()
+                        if closing_date < date.today():
+                            return Response({'Error': "Enter Valid Closing Date"},
+                                            status=status.HTTP_400_BAD_REQUEST)
+                    except:
+                        return Response({'Error': "Enter Valid Closing Date"},
+                                        status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    payload['closing_date'] = date.today().isoformat()
+
+        else:
+            if closing_date:
+                cur_stage = lead.stage
+                if cur_stage == StageConstant.OPPORTUNITY.name or cur_stage == StageConstant.CONTACTED .name or cur_stage == StageConstant.NEGOTIATION.name:
+                    return Response({'Error': "Only leads in Closed Won or Closed Lost stage can be provided a closing date."},
+                                    status=status.HTTP_400_BAD_REQUEST)
+
+                try:
+                    closing_date = datetime.strptime(
+                        closing_date, "%Y-%m-%d").date()
+                    if closing_date < date.today():
+                        return Response({'Error': "Enter Valid Closing Date"},
+                                        status=status.HTTP_400_BAD_REQUEST)
+                except:
+                    return Response({'Error': "Enter Valid Closing Date"},
+                                    status=status.HTTP_400_BAD_REQUEST)
+
+        # Confidence Check
+        if confidence:
+            try:
+                confidence = float(confidence)
+            except:
+                return Response({'Error': 'Invalid Confidence Level Provided'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            if confidence >= 0.0 and confidence <= 1.0:
+                payload['confidence'] = confidence
+            else:
+                return Response({'Error': 'Invalid Confidence Level Provided'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+        lead_serializer = LeadSerializer(lead, data=payload, partial=True)
+        if lead_serializer.is_valid():
+            lead = lead_serializer.save()
+            lead_json = lead_serializer.data
+            return Response({'data': lead_json},
+                            status=status.HTTP_200_OK)
+
+        else:
+            return Response(lead_serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    @auth_user
+    @auth_lead
+    def delete(self, request, user_dict, lead):
+        lead.delete()
+        return Response({'Message': 'Lead Deleted Successfully'},
+                        status=status.HTTP_200_OK)
