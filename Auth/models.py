3acf4d4e08e7988a18af2f5416befe58b09c4725
Vansh Kumar Sharma <vns.vansh123@gmail.com>
2023-05-26 11:12:56 +0530

    Made changes to Auth to support google login

--- Auth/models.py
+++ Auth/models.py
@@ -1,16 +1,13 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
 
 class LoginUser(models.Model):
     id=models.AutoField(primary_key=True)
     first_name=models.CharField(max_length=20, null=False)
     last_name=models.CharField(max_length=20, null=False)
     email=models.EmailField(null=False, unique=True)
-    phone_no=PhoneNumberField(unique = True, blank = True, null=True)
-    username=models.CharField(max_length=20, unique=True, null=False)
-    password=models.BinaryField(null=False,editable=True)
+    username=models.CharField(max_length=20, unique=True, null=True)
+    password=models.BinaryField(null=True,editable=True)
     
     def __str__(self) -> str:
         return f"Name - {self.first_name} {self.last_name} | Email - {self.email}"
