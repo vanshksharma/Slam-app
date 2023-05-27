e6f470272d9a691598b41c7403c83d285c47038d
Vansh Kumar Sharma <vns.vansh123@gmail.com>
2023-05-27 09:43:54 +0530

    Made Task start_date, due_date not null

--- Projects/models.py
+++ Projects/models.py
@@ -35,8 +35,8 @@ class Task(models.Model):
     status=models.CharField(max_length=15,
                               choices=[(tag.name,tag.value) for tag in StatusConstant],
                               default=StatusConstant.INCOMPLETE.name)
-    start_date=models.DateField(default=date.today)
-    due_date=models.DateField(null=True)
+    start_date=models.DateField()
+    due_date=models.DateField()
     
     def __str__(self) -> str:
         return f"Name - {self.name} | Project - {self.project.name}"
