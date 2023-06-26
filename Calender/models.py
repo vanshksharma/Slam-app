d54102c90a804090dc2b50b06350ea52f27fcba0
Vansh Kumar Sharma <vns.vansh123@gmail.com>
2023-06-26 17:09:54 +0530

    Database Changes Added Event start and due time

--- Calender/models.py
+++ Calender/models.py
@@ -9,8 +9,8 @@ class Event(models.Model):
     title=models.CharField(max_length=20,null=False)
     contact=models.ForeignKey(Contact,on_delete=models.CASCADE,db_column="contact")
     description=models.TextField(max_length=250,null=True)
-    start_date=models.DateField()
-    due_date=models.DateField()
+    start=models.DateTimeField()
+    due=models.DateTimeField()
     status=models.CharField(max_length=15,
                               choices=[(tag.name,tag.value) for tag in StatusConstant],
                               default=StatusConstant.INCOMPLETE.name)
