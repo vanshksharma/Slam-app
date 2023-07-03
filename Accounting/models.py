e2bfb010b21295b8f32618c3937cb436a9ddf0a2
Vansh Kumar Sharma <vns.vansh123@gmail.com>
2023-07-03 18:34:38 +0530

    Made Database Changes in Proposal, Invoice

--- Accounting/models.py
+++ Accounting/models.py
@@ -6,10 +6,14 @@ from Projects.models import Project
 
 class Proposal(models.Model):
     id=models.AutoField(primary_key=True)
-    contact=models.ForeignKey(Contact,on_delete=models.CASCADE,db_column="contact")      
-    amount=models.IntegerField(null=False)
-    date=models.DateField(default=date.today)
+    contact=models.ForeignKey(Contact,on_delete=models.CASCADE,db_column="contact")
+    proposal_number=models.CharField(max_length=30,null=False)
+    creation_date=models.DateField(default=date.today)
+    expiry_date=models.DateField()
     lead=models.ForeignKey(Lead, on_delete=models.SET_NULL, db_column="lead", null=True)
+    tax=models.IntegerField(null=True)
+    amount=models.IntegerField(null=False)
+    notes=models.TextField(null=True)
     
     def __str__(self) -> str:
         return f"Amount - {self.amount} | Contact - {self.contact.name}"
@@ -18,9 +22,13 @@ class Proposal(models.Model):
 class Invoice(models.Model):
     id=models.AutoField(primary_key=True)
     contact=models.ForeignKey(Contact,on_delete=models.CASCADE,db_column="contact")
-    amount=models.IntegerField(null=False)
-    date=models.DateField(default=date.today)
+    invoice_number=models.CharField(max_length=30,null=False)
+    creation_date=models.DateField(default=date.today)
+    expiry_date=models.DateField()
     project=models.ForeignKey(Project,on_delete=models.SET_NULL,db_column="project",null=True) # Incomplete or Complete
+    tax=models.IntegerField(null=True)
+    amount=models.IntegerField(null=False)
+    notes=models.TextField(null=True)
     
     def __str__(self) -> str:
         return f"Amount - {self.amount} | Contact - {self.contact.name}"
@@ -35,4 +43,16 @@ class Payment(models.Model):
     
     def __str__(self) -> str:
         return f"Amount - {self.amount_received} | Contact - {self.contact.name}"
+
+
+class Item(models.Model):
+    id=models.AutoField(primary_key=True)
+    details=models.CharField(max_length=100, null=False)
+    quantity=models.IntegerField(null=False)
+    rate=models.IntegerField(null=False)
+    amount=models.IntegerField(null=False)
+    proposal=models.ForeignKey(Proposal, on_delete=models.CASCADE, db_column="proposal", null=True)
+    invoice=models.ForeignKey(Invoice, on_delete=models.CASCADE, db_column="invoice", null=True)
     
+    def __str__(self) -> str:
+        return f"Details - {self.details} | Amount - {self.amount}"
