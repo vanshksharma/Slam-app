9278ab5813f358a559e596072d2a8298e9e967c6
Vansh Kumar Sharma <vns.vansh123@gmail.com>
2023-07-10 12:35:06 +0530

    Added description to task

--- Projects/models.py
+++ Projects/models.py
@@ -32,6 +32,7 @@ class Task(models.Model):
     user=models.ForeignKey(LoginUser,on_delete=models.CASCADE,db_column="user")
     contact=models.ForeignKey(Contact,on_delete=models.SET_NULL,db_column="contact",null=True)
     project=models.ForeignKey(Project,on_delete=models.SET_NULL,db_column="project",null=True)
+    description=models.TextField(null=True)
     priority=models.CharField(max_length=15,
                               choices=[(tag.name,tag.value) for tag in PriorityConstant],
                               default=PriorityConstant.LOW.name)
