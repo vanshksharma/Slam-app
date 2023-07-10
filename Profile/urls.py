ff259b2b5b6f463fbdee3782c2abed39bc6472e0
Vansh Kumar Sharma <vns.vansh123@gmail.com>
2023-07-10 20:01:56 +0530

    Made Integration GET API

--- Profile/urls.py
+++ Profile/urls.py
@@ -6,5 +6,6 @@ urlpatterns=[
     path('profile', views.ProfileHandler.as_view(), name = "Profile-api"),
     path('account', views.AccountHandler.as_view(), name = "Account-api"),
     path('integration/calender', views.CalenderIntegrationHandler.as_view(), name = "Calender-Integration-api"),
-    path('integration/zoom', views.ZoomIntegrationHandler.as_view(), name = "Zoom-Integration-api")
+    path('integration/zoom', views.ZoomIntegrationHandler.as_view(), name = "Zoom-Integration-api"),
+    path('integration', views.IntegrationHandler.as_view(), name = "Integration-api")
 ]
